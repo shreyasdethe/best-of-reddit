3823b4d8006d2d21ba62e747ad462d1e1c0f71c9
shreyasdethe <shreyasdethe9@gmail.com>
2020-08-15 16:13:10 +0530

    PWA Fixes

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ doXHR(u)
 
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-     navigator.serviceWorker.register('../sw.js').then( () => {
+     navigator.serviceWorker.register('sw.js').then( () => {
       console.log('Service Worker Registered')
      })
    })
